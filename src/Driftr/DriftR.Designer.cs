989716fa00066578576c5a5fe1c34e40969ed89a
Henk Mollema <henkmollema@gmail.com>
2014-10-09 17:29:51 +0200

    WIP on creating game canvas.

---
+++ src/Driftr/DriftR.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Driftr
+{
+    partial class Driftr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(998, 697);
+            this.screen.TabIndex = 0;
+            this.screen.TabStop = false;
+            // 
+            // Driftr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 697);
+            this.Controls.Add(this.screen);
+            this.Name = "Driftr";
+            this.Text = "Driftr";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Driftr_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.screen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox screen;
+
+    }
+}
+
---
+++ src/Driftr/Driftr.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Driftr
+{
+    public partial class Driftr : Form
+    {
+        private const float screenScale = 3.0F;
+        private Graphics _graphics;
+        private Bitmap _backbuffer;
+        private Size _bufferSize;
+        private Timer timer = new Timer();
+
+        private bool _left = false, _right = false, _up = false, _down = false;
+
+        private float steering = 0; // -1.0 is left, 0 is center,  1.0 is right.
+        private float throttle = 0; // 0 is coasting, 1 is full throttle.
+        private float breaks;   // 0 is no breaks, 1 is full breaks.
+
+        public Driftr()
+        {
+            InitializeComponent();
+            Application.Idle += Application_Idle;
+
+            screen.Paint += screen_Paint;
+            KeyUp += Driftr_KeyUp;
+            KeyDown += Driftr_KeyDown;
+
+            Init(screen.Size);
+        }
+
+        private void Driftr_Paint(object sender, PaintEventArgs e)
+        {
+            _backbuffer = new Bitmap(_bufferSize.Width, _bufferSize.Height);
+            _graphics = Graphics.FromImage(_backbuffer);
+        }
+
+        private void Init(Size size)
+        {
+
+        }
+
+        void screen_Paint(object sender, PaintEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Driftr_KeyUp(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Driftr_KeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
